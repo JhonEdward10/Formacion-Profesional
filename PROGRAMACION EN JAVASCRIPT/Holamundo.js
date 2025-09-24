7fc4ac588a8c1bc27fe6f7113de204daa6844d00
JhonEdward10 <jhonedwardmendeztovar@gmail.com>
2025-09-23 21:27:46 -0500

    Inicio de capitulo 2

--- PROGRAMACION EN JAVASCRIPT/Holamundo.js
+++ PROGRAMACION EN JAVASCRIPT/Holamundo.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo!");
+//Capitulo 1
+
+// console.log("Hola mundo!");
+// console.log("Jhon Edward Mendez Tovar");
+// console.log(2000);
+// console.log("abc", "def", "ghi");
+// console.log("Jhon Edward Mendez", "(2000)");
+// console.log("Jhon"+"Edward"+"Mendez"+"(2000)");
+
+//___________________________________________________________________________________________
+
+//Capitulo 2
+var height;
+console.log(height); // -> undefined
+// console.log(weight); // -> Uncaught ReferenceError: weight is not defined
