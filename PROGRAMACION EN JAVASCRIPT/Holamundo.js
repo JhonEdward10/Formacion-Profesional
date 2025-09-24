1052696f0f91ad37c08a024b88a58308744f324e
JhonEdward10 <jhonedwardmendeztovar@gmail.com>
2025-09-24 09:18:42 -0500

    Aprendiendo javaScript 2

--- PROGRAMACION EN JAVASCRIPT/Holamundo.js
+++ PROGRAMACION EN JAVASCRIPT/Holamundo.js
@@ -10,6 +10,61 @@
 //___________________________________________________________________________________________
 
 //Capitulo 2
-var height;
-console.log(height); // -> undefined
+// var height;
+// console.log(height); // -> undefined
 // console.log(weight); // -> Uncaught ReferenceError: weight is not defined
+
+// var height;
+// var height;
+// console.log(height);
+
+// let height;
+// let height;
+// console.log(height);
+
+// let height = 180;
+// let anotherHeight = height;
+// let weight;
+// console.log(height); // -> 180
+// console.log(anotherHeight); // -> 180
+// weight = 70;
+// console.log(weight); // -> 70
+
+// "use strict"
+// height = 180;
+// console.log(height); // -> 180
+
+// let steps = 100;
+// console.log(steps); // -> 100
+// steps = 120; 
+// console.log(steps);// -> 120
+// steps = steps + 200;
+// console.log(steps); // -> 320
+
+// let greeting = "Hello!";
+// let counter = 100;
+// greeting = greeting + counter;
+// console.log(greeting);
+
+// const greeting = "Hello!";
+// greeting = "Hi!";
+// console.log(greeting);
+
+// let counter; 
+// console.log(counter);
+// {
+//   counter = 1;
+//   console.log(counter);
+// }
+
+// counter = counter + 1;
+// console.log(counter);
+
+// let height = 180;
+// {
+//     let weight = 70;
+//     console.log(height);
+//     console.log(weight); 
+// }
+// console.log(height); 
+// console.log(weight); 
