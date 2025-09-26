9c26af742566effac1cce78063079c8980d33b3b
JhonEdward10 <jhonedwardmendeztovar@gmail.com>
2025-09-26 15:35:04 -0500

    segunda sesion javascript

--- PROGRAMACION EN JAVASCRIPT/Holamundo.js
+++ PROGRAMACION EN JAVASCRIPT/Holamundo.js
@@ -254,6 +254,18 @@
 // console.log(isDataValid); // -> true
 // console.log(typeof isDataValid); // -> boolean
 
+//Numeros
+const year = 1991;
+let delayInSeconds = 0.00016;
+let area = (16 * 3.14);
+let halfArea = area / 2;
+   
+console.log(year); // -> 1991;
+console.log(typeof year); // -> number;
+console.log(delayInSeconds);
+console.log(area);
+console.log(halfArea);
+console.log(typeof halfArea); // -> number;
 
 
-
+console.log("Hello");
