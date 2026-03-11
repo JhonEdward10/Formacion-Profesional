94f645fd467d0fdc58ba82e3dc596f0d2df5febd
Edward <jhonedwardmendeztovar@gmail.com>
2026-03-11 14:42:43 -0500

    Aprendiendo a configurar python

--- PROGRAMACION EN PYTHON/PROGRAMACION/Python/Introduction Proyects/holamundo.py
+++ PROGRAMACION EN PYTHON/PROGRAMACION/Python/Introduction Proyects/holamundo.py
@@ -4,3 +4,8 @@
 print("Hola Mundo")
 ###################################################
 
+#Diferencia entre intrepretado y compilado
+
+# Los objetivos de python es que es facil de entender, de codigo abierto, 
+# el codigo es comprensible como el ingles simple, 
+# adecuado para desarrollo de tareas cotidianas.
